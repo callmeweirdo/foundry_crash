8d3a2229b4d8674f5c06fc58b04afd731c9c576b
callmeweirdo <ogbandav102@gmail.com>
2024-02-02 21:05:01 +0100

    just fixed a bug ive had since for hours now

--- test/HelloWolrd.t.sol
+++ test/HelloWolrd.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {HelloWorld} from "../src/HelloWorld.sol";
 
-contract HelloWorldTest{
+contract HelloWorldTest is Test {
     HelloWorld helloWorld;
     function setUp () public {
         helloWorld = new HelloWorld();
